fb86f040a5fac982dd26bb7a64db38acd11a062a
Rene Czepluch <sepluk1@gmail.com>
2017-12-07 16:21:19 +0100

    cake

--- rapport/sammenlign.py
+++ rapport/sammenlign.py
@@ -1,6 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+params = {'legend.fontsize'     : '20',
+          'axes.labelsize'      : '20',
+          'axes.titlesize'      : '20',
+          'xtick.labelsize'     : '20',
+          'ytick.labelsize'     : '20',
+          'legend.numpoints'    : 1,
+          'text.latex.preamble' : [r'\usepackage{siunitx}',
+                                   r'\usepackage{amsmath}'],
+          'axes.spines.right'   : False,
+          'axes.spines.top'     : False,
+          'figure.figsize'      : [8.5, 6.375],
+          'legend.frameon'      : False
+          }
 
+plt.rcParams.update(params)
+plt.rc('text',usetex =True)
+plt.rc('font', **{'family' : "sans-serif"})
 # %% def constants
 n = 1
 a0 = 1
@@ -29,6 +45,6 @@ ax1.plot(r,asump(r),'b',color='grey', label='Eksakt')
 ax1.legend()
 plt.xlabel(r'$r$')
 plt.ylabel(r'$R(r)$')
-plt.ylim([-0.5,2.5])
-fig1.savefig("sammenligning.py")
+plt.ylim([-0.1,2.5])
+fig1.savefig("sammenligning.png")
 plt.show()
