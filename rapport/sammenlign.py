8470ebf7efeee0100bb65e6c7edbf1810062ae5d
Rene Czepluch <sepluk1@gmail.com>
2017-12-07 15:47:29 +0100

    test

--- rapport/sammenlign.py
+++ rapport/sammenlign.py
@@ -10,14 +10,15 @@ def asump(r):
     return (r/a0)**(n-1) * np.exp(-r/(n*a0))
 
 def func11(r):
-    del1 = (r/a0)**(n+np.sqrt(l*(l+1)))
+    led = n+np.sqrt(l*(l+1))
+    del1 = np.float_power((r/a0),(2.1))
     del2 = np.exp((1-(r/a0))/(n+np.sqrt(l*(l+1))))
     result = ((del1*del2)/r)
     return result
 
 # %% run functions in r interval
 interval = np.pi
-r = np.linspace(-interval,interval,200)
 
+r = np.linspace(-interval,interval,1)
 AssumpR = asump(r)
 function11 = func11(r)
