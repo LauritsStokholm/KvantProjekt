8722b6e138a7de2802e7206c50a22866c4cee68c
Rasmus Klitgaard <Rasmusklitgaard97@gmail.com>
2017-12-07 16:06:09 +0100

    Ewd

--- rapport/sammenlign.py
+++ rapport/sammenlign.py
@@ -11,7 +11,7 @@ def asump(r):
 
 def func11(r):
     led = n+np.sqrt(l*(l+1))
-    del1 = np.float_power((r/a0),(2.1))
+    del1 = np.power((r/a0),led)
     del2 = np.exp((1-(r/a0))/(n+np.sqrt(l*(l+1))))
     result = ((del1*del2)/r)
     return result
@@ -19,6 +19,15 @@ def func11(r):
 # %% run functions in r interval
 interval = np.pi
 
-r = np.linspace(-interval,interval,1)
+r = np.linspace(0,30,100000)
 AssumpR = asump(r)
 function11 = func11(r)
+
+fig1,ax1 = plt.subplots()
+ax1.plot(r,func11(r),'r',label='WKB')
+ax1.plot(r,asump(r),'b',label='Eksakt')
+ax1.legend()
+plt.xlabel(r'$r$')
+plt.ylabel(r'$R(r)$')
+plt.ylim([-0.5,2.5])
+plt.show()
